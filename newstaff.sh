6d154957ea5b4ff99e6d06e2a58bcd4fd967c4e4
RobertgSchultz8 <71945176+RobertgSchultz8@users.noreply.github.com>
2020-10-29 15:48:40 -0400

    Delete newstaff.sh

--- newstaff.sh
+++
@@ -1,7 +0,0 @@
-#This gives clear instructions for adding and naming a new user.
-echo -n "Enter name of new user. Name should consist of the first letter of the group they are in, their first name, and their last name."
-read new_user
-
-#I assume each group folder already contains the file for policies.
-sudo useradd new_user -G staff -k /etc/staff_skeleton -m
-sudo passwd new_user
