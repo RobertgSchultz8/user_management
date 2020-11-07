d1e97d536945a0661c73f2bd713276087f19bdf4
RobertgSchultz8 <71945176+RobertgSchultz8@users.noreply.github.com>
2020-11-07 11:44:23 -0500

    Create newaccounts.sh

---
+++ newaccounts.sh
@@ -0,0 +1,5 @@
+sed -e 's/[[:space:]]//g'
+sed -e 's,admn,:password:2000:2000:Admin:/etc/admin_skeleton:/bin/bash,'
+sed -e 's,dvlpr,:password:3000:3000:Developer:/etc/developer_skeleton:/bin/tcsh,'
+sed -e 's,stff,:password:4000:4000:Staff:/etc/staff_skeleton:/bin/bash,'
+sed -e 's,tmp,:password:5000:5000:Temp:/etc/temp_skeleton:/bin/bash,'
