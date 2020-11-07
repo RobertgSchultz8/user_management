b95b97db276d42cd624d2d24f02cd61b808c031c
RobertgSchultz8 <71945176+RobertgSchultz8@users.noreply.github.com>
2020-11-07 12:39:42 -0500

    Update newaccounts.sh

--- newaccounts.sh
+++ newaccounts.sh
@@ -1,9 +1,11 @@
 sed -e 's/[[:space:]]//g' UsersLvl1.txt > newusers1.txt
+cut -c -18 newusers1 > newusers1
 sed -e 's,admn,:password:2000:2000:Admin:/etc/admin_skeleton:/bin/bash,' newusers1.txt > newusers1.txt
 sed -e 's,dvlpr,:password:3000:3000:Developer:/etc/developer_skeleton:/bin/tcsh,' newusers1.txt > newusers1.txt
 sed -e 's,stff,:password:4000:4000:Staff:/etc/staff_skeleton:/bin/bash,' newusers1.txt > newusers1.txt
 sed -e 's,tmp,:password:5000:5000:Temp:/etc/temp_skeleton:/bin/bash,' newusers1.txt > newusers1.txt
 sed -e 's/[[:space:]]//g' UsersLvl2.txt > newusers2.txt
+cut -c -18 newusers2 > newusers2
 sed -e 's,admn,:password:2000:2000:Admin:/etc/admin_skeleton:/bin/bash,' newusers2.txt > newusers2.txt
 sed -e 's,dvlpr,:password:3000:3000:Developer:/etc/developer_skeleton:/bin/tcsh,' newusers2.txt > newusers2.txt
 sed -e 's,stff,:password:4000:4000:Staff:/etc/staff_skeleton:/bin/bash,' newusers2.txt > newusers2.txt
